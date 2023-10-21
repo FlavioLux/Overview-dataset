967cd5b01223ae9b4b14ca5fc30a4039c561d998
FlavioLux <42012471+FlavioLux@users.noreply.github.com>
2023-10-21 00:23:10 -0300

    Update titanic.py
    code

--- titanic.py
+++ titanic.py
@@ -1,1 +1,7 @@
+import pandas as pd
+from ydata_profiling import ProfileReport
+from IPython.display import display
 
+base = pd.read_csv("Titanic-Dataset.csv")
+base.columns = ["IDpassageiro", "Sobrevivente","ClassePassagem","Nome","Genero","idade","NrParentes","NrPaisFilhos","NrTicket","Tarifa","Cabine","PortoEmbarque"]
+display (base) 
